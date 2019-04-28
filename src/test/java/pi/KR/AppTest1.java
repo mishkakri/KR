51a38cf73a174c210fa695c4ea0df4168a5145be
ToxanUfa <anton.urk@mail.ru>
2019-04-28 17:23:40 +0500

    POm apptest1

--- src/test/java/pi/KR/AppTest1.java
+++ src/test/java/pi/KR/AppTest1.java
@@ -2,6 +2,7 @@ package pi.KR;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import junit.framework.*;
 
 /**
  * В теле данного класса расположен метод для проверки точности
